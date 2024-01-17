76e2fc787b3bce580ccb83623ea09ee49949220b
scotty974 <fabienetheve@hotmail.com>
2024-01-17 14:14:09 +0100

    Add config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('NestJs Full Api')
+    .setDescription('A NestJs backend example')
+    .setVersion('1.0')
+    .addTag('Routes')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.setGlobalPrefix('api')
   app.enableVersioning({
     type : VersioningType.URI
